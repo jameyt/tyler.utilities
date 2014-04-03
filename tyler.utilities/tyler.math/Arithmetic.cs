e7e7494f32d69b2a645a7ae5bbee9ee58bad95b9
Jamey Tyler <jameytyler@outlook.com>
2014-04-03 07:44:13 -0500

    Missing text project and arithmetic.cs

---
+++ tyler.utilities/tyler.math/Arithmetic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyler.math
+{
+    internal class Arithmetic
+    {
+
+        /// <summary>
+        /// Calculates the percent error of the actual value when compared to the expected value.
+        /// </summary>
+        /// <param name="actual">
+        /// Actual value
+        /// </param>
+        /// <param name="expected">
+        /// Expected value
+        /// </param>
+        /// <returns>
+        /// Error as a percentage.
+        /// </returns>
+        internal static double CalculatePercentError(double actual, double expected)
+        {
+            double percentError = 0.0;
+
+            if (expected.Equals(0))
+            {
+                if (actual > 0.001)
+                {
+                    percentError = (actual / 0.001) * 100;
+                }
+            }
+            else
+            {
+                percentError = System.Math.Abs(actual - expected) / expected * 100;
+            }
+            return percentError;
+        }
+
+    }
+}
